4aebd01c83c976e7cc543334a68e0c53cbdf5332
Hamza Zia <ziahamza2007@gmail.com>
2012-07-02 08:15:39 -0700

    server host used by default

--- js/script.js
+++ js/script.js
@@ -180,6 +180,9 @@ var update_ui = function() {
 
 $(function() {
 	get_conf_cookie();
+	if (window.location.protocol === "http:") {
+		server_conf.host = window.location.host;
+	}
 	var modal_conf = {
 		show: false,
 		backdrop: false
