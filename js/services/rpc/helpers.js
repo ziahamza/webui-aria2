9c378670dd8e26927e5bf4cfdf85e295469d4aa3
Florent Viard <fviard@lacie.com>
2014-02-27 01:19:05 +0100

    Added "Advanced options" collapsable entries in getUris/getTorrents/getMetalinks modals.
    Added "Advanced options" block in addXXXX modals and one "dir" option for manually setup a destination folder for a particular added download.
    Now, more advanced options could easily be added.

--- js/services/rpc/helpers.js
+++ js/services/rpc/helpers.js
@@ -11,28 +11,28 @@ angular.module('webui.services.rpc.helpers', [
     getAria2Version: function() {
       return miscellaneous.version;
     },
-    addUris: function(uris) {
+    addUris: function(uris, advanced_opts) {
       _.each(uris, function(uri) {
         // passing true to batch all the addUri calls
-        rpc.once('addUri', [uri], angular.noop, true);
+        rpc.once('addUri', [uri, advanced_opts], angular.noop, true);
       });
 
       // now dispatch all addUri syscalls
       rpc.forceUpdate();
     },
-    addTorrents: function(txts) {
+    addTorrents: function(txts, advanced_opts) {
       _.each(txts, function(txt) {
-        // passing true to batch all the addUri calls
-        rpc.once('addTorrent', [txt], angular.noop, true);
+        // passing true to batch all the addTorrents calls
+        rpc.once('addTorrent', [txt, [], advanced_opts], angular.noop, true);
       });
 
       // now dispatch all addUri syscalls
       rpc.forceUpdate();
     },
-    addMetalinks: function(txts) {
+    addMetalinks: function(txts, advanced_opts) {
       _.each(txts, function(txt) {
-        // passing true to batch all the addUri calls
-        rpc.once('addMetalink', [txt], angular.noop, true);
+        // passing true to batch all the addMetalinks calls
+        rpc.once('addMetalink', [txt, advanced_opts], angular.noop, true);
       });
 
       // now dispatch all addUri syscalls
