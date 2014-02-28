f816ab853fa3d6f5c927a2f97706038f55bdb58f
Nils Maier <maierman@web.de>
2014-02-28 12:39:34 +0100

    Safari compat

--- js/init.js
+++ js/init.js
@@ -13,5 +13,14 @@ var webui = angular.module('webui', [
 ]);
 
 $(function() {
+  if (!String.prototype.startsWith) {
+    Object.defineProperty(String.prototype, 'startsWith', {
+          enumerable: false, configurable: false, writable: false,
+          value: function (searchString, position) {
+            position = position || 0;
+            return this.indexOf(searchString, position) === position;
+          }
+    });
+  }
   angular.bootstrap(document, ['webui'])
 });
