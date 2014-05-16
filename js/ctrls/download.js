7eec859d497ef43999ffca38e801bd8fc98056ca
Nils Maier <maierman@web.de>
2014-05-16 03:38:43 +0200

    Do not remove stuff from lists...
    next update will take care of that. Not removing will avoid re-insertion
    if aria2/RPC did not yet remove the item before the next update.

--- js/ctrls/download.js
+++ js/ctrls/download.js
@@ -52,17 +52,6 @@ function(
       d.followedFrom = null;
     }
     rpc.once(method, [d.gid], cb);
-
-    var lists = [scope.active, scope.waiting, scope.stopped], ind = -1, i;
-    for (var i = 0; i < lists.length; ++i) {
-      var list = lists[i];
-      var idx = list.indexOf(d);
-      if (idx < 0) {
-        continue;
-      }
-      list.splice(idx, 1);
-      return;
-    }
   }
 
   scope.restart = function(d) {
